898aa84039a548eead689802ecf04d8f49bfab11
Danizinh <danimaltoc@gmail.com>
2022-12-13 16:11:26 -0300

    news modification

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ resert: true })
+
+sr.reveal('.content', { durantion: 4000 })
+
+sr.reveal('.content-img',{
+    rotate:{ x:0, y:80, z:0},
+    durantion:2000,
+})
+
+sr.reveal('.paragraph',{durantion:9000})
+sr.reveal('.btn',{durantion:1000})
